a84cb40069d0ca9c9104e4347d1292d3dd976d37
obaidrehman30 <obaidrehman3009@gmail.com>
2025-08-13 16:35:51 -0400

    Update app

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -195,13 +195,6 @@
     });
   };
 
-  // Save list of cities to localStorage, see note below about localStorage.
-  app.saveSelectedCities = function() {
-    var selectedCities = JSON.stringify(app.selectedCities);
-    // IMPORTANT: See notes about use of localStorage.
-    localStorage.selectedCities = selectedCities;
-  };
-
   /************************************************************************
    *
    * Code required to start the app
